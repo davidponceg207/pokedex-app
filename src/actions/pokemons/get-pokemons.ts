b037cbc05fce046f717f76b55c00b1c3cf60dbd3
David <davidponceg207@gmail.com>
2026-01-15 12:52:11 -0600

    PokeAPI interfaces

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -1,16 +1,26 @@
 import { pokeApi } from "../../config/api/pokeApi";
-import { Pokemon } from "../../domain/entities/pokemon";
+import type { Pokemon } from "../../domain/entities/pokemon";
+import type { PokeAPIPaginatedResponse, PokeAPIPokemon } from "../../infrastructure/interfaces/pokeapi.interfaces";
 
-export const getPokemons = async ():Promise<Pokemon[]> => {
+export const getPokemons = async (page: number, limit: number = 20):Promise<Pokemon[]> => {
 
     try {
 
-        const url = '/pokemon';
+        const url = `/pokemon?offset=${page * 10}&limit=${limit}`;
 
-        const { data } = await pokeApi.get(url);
+        const { data } = await pokeApi.get<PokeAPIPaginatedResponse>(url);
+
+        const pokemonPromises = data.results.map((info) => {
+            return pokeApi.get<PokeAPIPokemon>(info.url)
+        });
+
+        const pokeApiPokemons = await Promise.all(pokemonPromises);
+
+        console.log(pokeApiPokemons);
 
         console.log(data);
         return [];
+
     } catch (error) {
         throw new Error('Error getting pokemons :(');
     }
