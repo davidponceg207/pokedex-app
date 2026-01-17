2b3838539c6c96fdd950f970867c40babefcdf5a
David <davidponceg207@gmail.com>
2026-01-16 21:03:08 -0600

    Query client

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import { FlatList, StyleSheet, View } from 'react-native';
-import { ActivityIndicator, Text } from 'react-native-paper';
+import { Text } from 'react-native-paper';
 import { getPokemons } from '../../../actions/pokemons';
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query';
 import { PokeballBg } from '../../components/ui/PokeballBg';
 import { globalTheme } from '../../../config/theme/global-theme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -10,6 +10,7 @@ import { PokemonCard } from '../../components/pokemons/PokemonCard';
 export const HomeScreen = () => {
 
     const { top } = useSafeAreaInsets();
+    const queryClient = useQueryClient();
 
     // * Traditional http
     // const { isLoading, data: pokemons = []} = useQuery({
@@ -21,9 +22,16 @@ export const HomeScreen = () => {
     const { isLoading, data, fetchNextPage} = useInfiniteQuery({
         queryKey: ['pokemons', 'infinite'],
         initialPageParam: 0,
-        queryFn: (params) => getPokemons(params.pageParam),
+        queryFn: async (params) => {
+            const pokemons = await getPokemons(params.pageParam);
+            pokemons.forEach(pokemon => {
+                queryClient.setQueryData(['pokemon', pokemon.id], pokemon)
+            });
+            return pokemons
+        },
         getNextPageParam: (lastPage, pages) => pages.length,
         staleTime: 1000 * 60 * 60
+
     });
 
     return (
