632cf46d00650c2827e286e8f94d457fb8081cb4
David <davidponceg207@gmail.com>
2026-01-16 19:41:04 -0600

    Infinite pokemon scroll

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import { FlatList, StyleSheet, View } from 'react-native';
 import { ActivityIndicator, Text } from 'react-native-paper';
 import { getPokemons } from '../../../actions/pokemons';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { PokeballBg } from '../../components/ui/PokeballBg';
 import { globalTheme } from '../../../config/theme/global-theme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -11,9 +11,18 @@ export const HomeScreen = () => {
 
     const { top } = useSafeAreaInsets();
 
-    const { isLoading, data: pokemons = []} = useQuery({
-        queryKey: ['pokemons'],
-        queryFn: () => getPokemons(0),
+    // * Traditional http
+    // const { isLoading, data: pokemons = []} = useQuery({
+    //     queryKey: ['pokemons'],
+    //     queryFn: () => getPokemons(0),
+    //     staleTime: 1000 * 60 * 60
+    // });
+
+    const { isLoading, data, fetchNextPage} = useInfiniteQuery({
+        queryKey: ['pokemons', 'infinite'],
+        initialPageParam: 0,
+        queryFn: (params) => getPokemons(params.pageParam),
+        getNextPageParam: (lastPage, pages) => pages.length,
         staleTime: 1000 * 60 * 60
     });
 
@@ -23,7 +32,7 @@ export const HomeScreen = () => {
             <PokeballBg style={styles.imgPosition}/>
 
             <FlatList
-                data={ pokemons }
+                data={ data?.pages.flat() ?? [] }
                 keyExtractor={ (pokemon, index) => `${pokemon.id}-${index}` }
                 numColumns={2}
                 style={{paddingTop: top + 20}}
@@ -32,6 +41,8 @@ export const HomeScreen = () => {
                 )}
                 renderItem={({item}) => <PokemonCard pokemon={item} />}
                 showsVerticalScrollIndicator={false}
+                onEndReachedThreshold={0.6}
+                onEndReached={() => fetchNextPage()}
             />
         </View>
     )
